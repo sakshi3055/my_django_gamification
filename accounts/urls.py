294338105d689cf4d31ed030fc89ddc545a9e8c6
sakshi tiwari <tiwarisakshi572@gmail.com>
2024-04-19 10:14:25 +0530

    new stuff

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("s/login", views.student_login, name="slogin"),
+    path("s/register", views.student_register, name="sregister"),
+    path('logout', views.logout_view, name='logout'),
+]
