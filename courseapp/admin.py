f02099676a67aebb3b275bab68d032bc12a025b9
sakshi tiwari <tiwarisakshi572@gmail.com>
2024-05-09 11:12:46 +0530

    stuff

--- courseapp/admin.py
+++ courseapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Lesson, Badge, Quiz
+from .models import Lesson, Badge, Quiz, QuizCompletion, BadgeAssignment, LessonCompletion
 
 # Register your models here.
 admin.site.register(Lesson)
 admin.site.register(Badge)
 admin.site.register(Quiz)
+admin.site.register(QuizCompletion)
+admin.site.register(BadgeAssignment)
+admin.site.register(LessonCompletion)
